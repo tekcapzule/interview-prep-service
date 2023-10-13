017be5c1a6611d5ba0080a52dbfb95608aa4a349
haritha <haritha.peryala@gmail.com>
2023-10-13 22:12:29 +0530

    recommend

---
+++ application/src/main/java/com/tekcapsule/interviewprep/application/function/RecommendFunction.java
@@ -0,0 +1,58 @@
+package com.tekcapsule.interviewprep.application.function;
+
+import com.tekcapsule.core.domain.Origin;
+import com.tekcapsule.core.utils.HeaderUtil;
+import com.tekcapsule.core.utils.Outcome;
+import com.tekcapsule.core.utils.PayloadUtil;
+import com.tekcapsule.core.utils.Stage;
+import com.tekcapsule.interviewprep.application.config.AppConfig;
+import com.tekcapsule.interviewprep.application.function.input.RecommendInput;
+import com.tekcapsule.interviewprep.application.mapper.InputOutputMapper;
+import com.tekcapsule.interviewprep.domain.command.RecommendCommand;
+import com.tekcapsule.interviewprep.domain.service.InterviewPrepService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.GenericMessage;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+
+@Component
+@Slf4j
+public class RecommendFunction implements Function<Message<RecommendInput>, Message<Void>> {
+
+    private final InterviewPrepService interviewPrepService;
+
+    private final AppConfig appConfig;
+
+    public RecommendFunction(final InterviewPrepService interviewPrepService, final AppConfig appConfig) {
+        this.interviewPrepService = interviewPrepService;
+        this.appConfig = appConfig;
+    }
+
+
+    @Override
+    public Message<Void> apply(Message<RecommendInput> recommendInputMessage) {
+        Map<String, Object> responseHeaders = new HashMap<>();
+        Map<String, Object> payload = new HashMap<>();
+        String stage = appConfig.getStage().toUpperCase();
+        try {
+            RecommendInput recommendInput = recommendInputMessage.getPayload();
+            log.info(String.format("Entering recommend interviewprep Function -  course ID:%s", recommendInput.getCourseId()));
+            Origin origin = HeaderUtil.buildOriginFromHeaders(recommendInputMessage.getHeaders());
+            RecommendCommand recommendCommand = InputOutputMapper.buildRecommendCommandFromRecommendInput.apply(recommendInput, origin);
+            interviewPrepService.recommend(recommendCommand);
+            responseHeaders = HeaderUtil.populateResponseHeaders(responseHeaders, Stage.valueOf(stage), Outcome.SUCCESS);
+            payload = PayloadUtil.composePayload(Outcome.SUCCESS);
+        } catch (Exception ex) {
+            log.error(ex.getMessage());
+            responseHeaders = HeaderUtil.populateResponseHeaders(responseHeaders, Stage.valueOf(stage), Outcome.ERROR);
+            payload = PayloadUtil.composePayload(Outcome.ERROR);
+        }
+        return new GenericMessage(payload, responseHeaders);
+
+    }
+}
